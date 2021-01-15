cf77587ddad355faaf7bf38872841d30c3746324
Simratt <47543274+Simratt@users.noreply.github.com>
2021-01-15 16:57:19 -0500

    working model

--- particle.py
+++ particle.py
@@ -1,39 +1,88 @@
 import pygame 
+import math
+import random
+import numpy as np
+GRAVITY = 0.01
 
 class Particle:
     ''' This is a representation of a particle in space
 
     === Attributes ===
-    - _x: the x position of the particle 
-    - _y: the y position of the particle 
+    - x: the x position of the particle 
+    - y: the y position of the particle 
     - coords: the coordinates of the particle on the screen
     - size: the radius of the particle 
     - color: the color of the particle 
-    - thickness: the thickness of the particle, this is for the pygame window
+    - mass: the mass of the particle, we use this to also determine the sizes
+            of the particle for the pygame window
+    - velocity: the velocity of the partilce, starts off with [0,0] and then 
+                addys the velocity to <x> <y>
+    - dv: the change in velocity
+    - thicknessL: used by pygame to determine how thick the stroke of the drawn 
+                  circle should be
 
     === Representaion Invariants === 
     #TODO 
 
-    _x: int 
-    _y: int 
-    coords: tuple(int, int)
+    x: int 
+    y: int 
+    coords: numpy array
     size: [int, float]
     color: tuple(int,int,int) 
-    thickness: int 
+    mass: int 
+    velocity: numpy array
+    rot: float
+    dv: numpy array
+    thickness: int
     '''
 
-    def __init__(self, x: int ,y: int, size: int) -> None:
+    def __init__(self, x: int ,y: int, mass:float) -> None:
         ''' the constructor for the particle class'''
         
-        self._x = x
-        self._y = y
-        self.coords = (self._x, self._y) 
-        self.size = size
+        self.x = x
+        self.y = y
+        self.coords = np.array([self.x,self.y], dtype= float)
+        self.velocity = np.array([0,0], dtype=float)
+        self.mass = mass
+        self.size = (self.mass/math.pi)**(1/2)
+        self.FG = 0
         self.color = (255,255,255)
-        self.thickness = 1
-
+        self.thickness = 100
+        
     def display(self, screen: pygame.display) -> None:
         ''' this function draws the particle onto a paygame window'''
-        
         pygame.draw.circle(screen, self.color, self.coords, self.size, self.thickness)
 
+
+    def gravitaional_force(self, other):
+
+        if self == other:
+            self.dv = np.array([0,0], dtype = float)
+        else:
+            dist = math.sqrt((self.x-other.x)**2 + (self.y-other.y)**2)
+            if dist == 0:
+                self.dv = 0 #acceleration is zero if the distance between the two objects is 0
+            else:
+                self.FG = (-GRAVITY*(self.mass*other.mass)/dist**2) * ((self.coords-other.coords)/dist)
+                self.dv = self.FG / self.mass #Fnet = ma therfore a = Fnet/m
+                # to preserve the quality of inelastic collisions we change <self.dv> to reverse if it 
+                # becomes too close to another particle
+                if dist < self.size  + other.size: 
+                    self.dv *= -0.5
+
+        self.velocity += self.dv #add the force vector to the velocity 
+        self._displace(self.velocity)
+
+    def _displace(self, vec:tuple) -> None:
+        '''moves the particle by <vec> distance'''
+        self.x += vec[0]
+        self.y += vec[1]   
+        self.coords = np.array([self.x, self.y], dtype= float) 
+    
+    def check_bounds(self, screen:list) -> None:
+        ''' this function flips the direction of this particle'''
+        if 0 < self.x < screen[0] and 0 < self.y < screen[1] :
+            pass 
+        else:
+            self.velocity *= -0.5
+            self._displace(self.velocity)
--- window.py
+++ window.py
@@ -1,19 +1,45 @@
 import sys, pygame
-from particle import Particle
+from particle import *
+
+# some preliminary functions 
+def randomize(_range:tuple) -> tuple:
+    ''' returns a random set of coordinates within <range>'''
+    coords = (random.randint(0, _range[0]), random.randint(0,_range[1]))
+    return coords
+
+
+particles = []
 
 background_color = (0,0,0)
-(width, height) = (300, 200)
+(width, height) = (600, 600)
 screen = pygame.display.set_mode((width, height))
-pygame.display.set_caption('Abhay is a samosa')
+pygame.display.set_caption('N Body Simulation')
 screen.fill(background_color)
 
-p = Particle(150, 50, 15)
-p.display(screen)
+#creating the particles 
 
+for i in range(3):
+    coords = randomize((width, height))
+    p = Particle(coords[0], coords[1], random.randint(10, 100))
+    p.display(screen)
+    particles.append(p)
+
+p.display(screen)
 pygame.display.flip() # this puts all the stuff on the screen 
 
 running = True 
 while running:
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+    
+    screen.fill(background_color)
+    for p_i in particles:
+        for p_f in particles:
+            p_i.gravitaional_force(p_f)
+            p_i.check_bounds([width, height])
+            p_i.display(screen)
+    pygame.display.flip()
+
+
